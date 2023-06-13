cb596e3941993674c502d18de71f15844ded934c
oxyde9 <63107264+oxyde9@users.noreply.github.com>
2023-06-13 12:08:26 +0200

    second commit generate partners ajout de l'image

--- app/src/main/java/com/libertytech/tinderlike/MainActivity.kt
+++ app/src/main/java/com/libertytech/tinderlike/MainActivity.kt
@@ -18,9 +18,9 @@ class MainActivity : ComponentActivity() {
 
         // Crée une liste de partenaires
         val listePartenaires = mutableListOf(
-            Partenaire("Alice", 25, "J'aime les voyages et les chiens."),
-            Partenaire("Bob", 30, "Je suis passionné de photographie."),
-            Partenaire("Caroline", 28, "J'adore la musique et la danse.")
+            Partenaire("Alice", 25, "J'aime les voyages et les chiens.", "https://static-cse.canva.com/blob/605714/canva_photo_profil_reseaux_sociaux.jpg"),
+            Partenaire("Bob", 30, "Je suis passionné de photographie.", "https://www.jeancoutu.com/globalassets/revamp/photo/conseils-photo/20160302-01-reseaux-sociaux-profil/photo-profil_301783868.jpg"),
+            Partenaire("Caroline", 28, "J'adore la musique et la danse.", "https://blog.neocamino.com/wp-content/uploads/2014/06/photo-profil.jpg")
         )
 
         // Affiche les noms des partenaires dans la console
@@ -38,7 +38,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class Partenaire(val nom: String, val age: Int, val description: String)
+data class Partenaire(val nom: String, val age: Int, val description: String, val pictureUrl: String)
+
 
 @Composable
 fun Greeting(name: String) {
