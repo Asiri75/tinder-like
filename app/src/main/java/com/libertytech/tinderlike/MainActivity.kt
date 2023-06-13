f74a214aff3a620b40f7af1f15899ec6fa0d1598
oxyde9 <63107264+oxyde9@users.noreply.github.com>
2023-06-13 15:08:32 +0200

    third commit generate partners ajout de l'image

--- app/src/main/java/com/libertytech/tinderlike/MainActivity.kt
+++ app/src/main/java/com/libertytech/tinderlike/MainActivity.kt
@@ -10,31 +10,66 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import com.libertytech.tinderlike.model.Partenaire
 import com.libertytech.tinderlike.ui.theme.TinderLikeTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class MainActivity : ComponentActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
+    private val partenairesCollection = db.collection("partenaires") // Remplace "partenaires" par le nom de ta collection dans Firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             TinderLikeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    FetchPartenaires()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun FetchPartenaires() {
+        val partenaires = mutableListOf<Partenaire>()
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TinderLikeTheme {
-        Greeting("Android")
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val querySnapshot: QuerySnapshot = partenairesCollection.get().await()
+                val documents = querySnapshot.documents
+
+                for (document in documents) {
+                    val nom = document.getString("nom") ?: ""
+                    val age = document.getLong("age")?.toInt() ?: 0
+                    val description = document.getString("description") ?: ""
+                    val pictureUrl = document.getString("pictureUrl") ?: ""
+
+                    val partenaire = Partenaire(nom, age, description, pictureUrl)
+                    partenaires.add(partenaire)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        // Affiche la liste des partenaires récupérés depuis Firebase
+        for (partenaire in partenaires) {
+            Text(text = "Nom: ${partenaire.nom}, Age: ${partenaire.age}, Description: ${partenaire.description}")
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        TinderLikeTheme {
+            FetchPartenaires()
+        }
     }
 }
