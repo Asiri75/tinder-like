a7e7541cdf1b572711d0977e2bf95c0b36b9a3ee
oxyde9 <63107264+oxyde9@users.noreply.github.com>
2023-06-13 14:43:01 +0200

    second commit generate partners ajout de l'image

--- app/src/main/java/com/libertytech/tinderlike/MainActivity.kt
+++ app/src/main/java/com/libertytech/tinderlike/MainActivity.kt
@@ -10,14 +10,27 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.libertytech.tinderlike.model.Partenaire
 import com.libertytech.tinderlike.ui.theme.TinderLikeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Crée une liste de partenaires
+        val listePartenaires = mutableListOf(
+            Partenaire("Alice", 25, "J'aime les voyages et les chiens.", "https://static-cse.canva.com/blob/605714/canva_photo_profil_reseaux_sociaux.jpg"),
+            Partenaire("Bob", 30, "Je suis passionné de photographie.", "https://www.jeancoutu.com/globalassets/revamp/photo/conseils-photo/20160302-01-reseaux-sociaux-profil/photo-profil_301783868.jpg"),
+            Partenaire("Caroline", 28, "J'adore la musique et la danse.", "https://blog.neocamino.com/wp-content/uploads/2014/06/photo-profil.jpg")
+        )
+
+        // Affiche les noms des partenaires dans la console
+        for (partenaire in listePartenaires) {
+            println(partenaire.nom)
+        }
+
         setContent {
             TinderLikeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
                     Greeting("Android")
                 }
@@ -25,6 +38,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String) {
---
+++ app/src/main/java/com/libertytech/tinderlike/model/Partenaire.kt
@@ -0,0 +1,8 @@
+package com.libertytech.tinderlike.model
+
+data class Partenaire (
+    val nom: String,
+    val age: Int,
+    val description: String,
+    val pictureUrl: String
+        )
