05852578767ca2183fad5c04a6d86712113ceeb1
Lukas Descoins <lukas@bailfacile.fr>
2023-06-13 12:07:54 +0200

    Add Login and Register to AuthRepository

--- app/src/main/java/com/libertytech/tinderlike/repositories/AuthRepository.kt
+++ app/src/main/java/com/libertytech/tinderlike/repositories/AuthRepository.kt
@@ -1,4 +1,20 @@
 package com.libertytech.tinderlike.repositories
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 class AuthRepository {
+
+    private var auth: FirebaseAuth = Firebase.auth
+    suspend fun Register(email: String, password: String): FirebaseUser? {
+        auth.createUserWithEmailAndPassword(email, password)
+        return auth.currentUser
+    }
+
+    suspend fun Login(email: String, password: String): FirebaseUser? {
+        auth.signInWithEmailAndPassword(email, password)
+        return auth.currentUser
+    }
 }
