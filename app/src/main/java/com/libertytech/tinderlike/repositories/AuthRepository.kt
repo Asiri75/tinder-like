dd73133f3c6bc13eb0cfb7c75dc7fc39e776e688
Lukas Descoins <lukas@bailfacile.fr>
2023-06-13 12:18:26 +0200

    :art: UserIsAuth UseCase

--- app/src/main/java/com/libertytech/tinderlike/repositories/AuthRepository.kt
+++ app/src/main/java/com/libertytech/tinderlike/repositories/AuthRepository.kt
@@ -7,7 +7,8 @@ import com.google.firebase.ktx.Firebase
 
 class AuthRepository {
     private var auth: FirebaseAuth = Firebase.auth
-    suspend fun callFirebaseIsAuth (): FirebaseUser? {
+
+    suspend fun userIsAuth (): FirebaseUser? {
         return try {
             auth.currentUser
         }catch (e: Exception){
@@ -16,13 +17,12 @@ class AuthRepository {
         }
     }
 
-
-    suspend fun Register(email: String, password: String): FirebaseUser? {
+    suspend fun register(email: String, password: String): FirebaseUser? {
         auth.createUserWithEmailAndPassword(email, password)
         return auth.currentUser
     }
 
-    suspend fun Login(email: String, password: String): FirebaseUser? {
+    suspend fun login(email: String, password: String): FirebaseUser? {
         auth.signInWithEmailAndPassword(email, password)
         return auth.currentUser
     }
