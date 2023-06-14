12bd53800acac00f9ca03bc140dd97357d6845b8
oxyde9 <63107264+oxyde9@users.noreply.github.com>
2023-06-14 12:23:00 +0200

    eight commit generate partners with buttons

--- app/src/main/java/com/libertytech/tinderlike/screens/parteners/PartenersScreen.kt
+++ app/src/main/java/com/libertytech/tinderlike/screens/parteners/PartenersScreen.kt
@@ -1,7 +1,9 @@
 package com.libertytech.tinderlike.screens.parteners
+import androidx.compose.foundation.layout.Arrangement
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
@@ -29,7 +32,6 @@ fun PartenairesScreen(viewModel: PartenairesViewModel = viewModel()) {
     LaunchedEffect(Unit) {
         viewModel.fetchPartenaires()
     }
-
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -71,14 +73,35 @@ fun PartenairesScreen(viewModel: PartenairesViewModel = viewModel()) {
                                     text = partenaire.description,
                                     style = MaterialTheme.typography.body1
                                 )
+
+                                Spacer(modifier = Modifier.height(16.dp))
+
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    Button(
+                                        onClick = { /* Action pour le like */ },
+                                        modifier = Modifier.padding(end = 8.dp)
+                                    ) {
+                                        Text(text = "Like")
+                                    }
+
+                                    Button(
+                                        onClick = { /* Action pour le dislike */ },
+                                        modifier = Modifier.padding(start = 8.dp)
+                                    ) {
+                                        Text(text = "Dislike")
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
         } else {
-
             CircularProgressIndicator()
         }
     }
 }
+
